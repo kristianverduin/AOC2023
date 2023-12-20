45bbe6eb30feedc3b986468ef01feb3e1006f3a2
kristianverduin <kristianverduin@gmail.com>
2023-12-20 13:13:43 +0100

    Part 1 working

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -1,6 +1,6 @@
 
 def readInput():
-    with open("test.txt", 'r') as file:
+    with open("input.txt", 'r') as file:
         text = file.read().strip()
     return text.split('\n\n')
 
@@ -8,17 +8,24 @@ def splitInstruction(instruction):
     values = instruction[1].split(':')
     return instruction[0], values[0], values[1]
 
-def readWorkflows(workflows, values):
-    instructions = workflows['in'].split(',')
+def readWorkflows(workflow, workflows, values):
+    if workflow == 'A':
+        return True
+    elif workflow == 'R':
+        return False
+    instructions = workflows[workflow].split(',')
     for instruction in instructions:
         split = ''
         if '<' in instruction:
             split = splitInstruction(instruction.split('<'))
+            if values[split[0]] < int(split[1]):
+                return readWorkflows(split[2], workflows, values)
         elif '>' in instruction:
             split = splitInstruction(instruction.split('>'))
+            if values[split[0]] > int(split[1]):
+                return readWorkflows(split[2], workflows, values)
         else:
-            split = instruction
-        print(split)
+            return readWorkflows(instruction, workflows, values)
 
 text = readInput()
 workflows = text[0]
@@ -29,11 +36,14 @@ for workflow in workflows.split('\n'):
     name, workflow = workflow.split('{')
     workflowDict[name] = workflow.strip('}')
 
+result = 0
 for rating in ratings.split('\n'):
     valueDict = {}
     values = rating.strip('\{\}').split(',')
     for value in values:
         part, number = value.split('=')
         valueDict[part] = int(number)
-    readWorkflows(workflowDict, valueDict)
-    exit()
+    if readWorkflows('in', workflowDict, valueDict):
+       result += sum(valueDict.values())
+
+print(result)
