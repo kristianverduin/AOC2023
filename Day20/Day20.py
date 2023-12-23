94282fd9746e51987aa21394635f58bda24eb067
kristianverduin <kristianverduin@gmail.com>
2023-12-23 19:52:26 +0100

    Day 20 part 2

--- Day20/Day20.py
+++ Day20/Day20.py
@@ -44,40 +44,96 @@ def allOff(mods):
                 return False
     return True
 
-lines = readInput()
-modules = parseInput(lines)
+def partOne():
+    lines = readInput()
+    modules = parseInput(lines)
 
-# [lowPulseCount, highPulseCount]
-pulseCount = [0, 0]
-cycle = 0
+    # [lowPulseCount, highPulseCount]
+    pulseCount = [0, 0]
+    cycle = 0
 
-while cycle < 1000:
-    cycle += 1
+    while cycle < 1000:
+        cycle += 1
 
-    # Pulse to broadcaster and all destinations
-    pulseCount[0] += len(modules['broadcaster']['dest']) + 1
-    
-    queue = [(modules['broadcaster']['dest'], 0, 'broadcaster')]
-    while queue:
-        dests, pulse, source = queue.pop(0)
+        # Initial pulse to broadcaster and all destinations
+        pulseCount[0] += len(modules['broadcaster']['dest']) + 1
+        
+        queue = [(modules['broadcaster']['dest'], 0, 'broadcaster')]
+        while queue:
+            dests, pulse, source = queue.pop(0)
 
-        for dest in dests:
-            if dest not in modules:
-                continue
+            for dest in dests:
+                if dest not in modules:
+                    continue
 
-            if modules[dest]['type'] == '%':
-                if pulse == 0:
-                    modules[dest]['switch'] = not modules[dest]['switch']
-                    pulseType = int(modules[dest]['switch'])
+                if modules[dest]['type'] == '%':
+                    if pulse == 0:
+                        modules[dest]['switch'] = not modules[dest]['switch']
+                        pulseType = int(modules[dest]['switch'])
+                        pulseCount[pulseType] += len(modules[dest]['dest'])
+                        queue.append((modules[dest]['dest'], pulseType, dest))
+                elif modules[dest]['type'] == '&':
+                    modules[dest]['inputs'][source] = pulse
+                    pulseType = 1 - all(modules[dest]['inputs'].values())
                     pulseCount[pulseType] += len(modules[dest]['dest'])
                     queue.append((modules[dest]['dest'], pulseType, dest))
-            elif modules[dest]['type'] == '&':
-                modules[dest]['inputs'][source] = pulse
-                pulseType = 1 - all(modules[dest]['inputs'].values())
-                pulseCount[pulseType] += len(modules[dest]['dest'])
-                queue.append((modules[dest]['dest'], pulseType, dest))
 
-    if allOff(modules):
-        break
+        if allOff(modules):
+            break
 
-print(((1000//cycle) ** 2) * math.prod(pulseCount))
+    return ((1000//cycle) ** 2) * math.prod(pulseCount)
+
+def partTwo():
+    lines = readInput()
+    modules = parseInput(lines)
+
+    # [lowPulseCount, highPulseCount]
+    pulseCount = [0, 0]
+    cycle = 0
+
+    rxParents = set([mod for mod, value in modules.items() if 'rx' in value['dest']])
+    expectedPulse = 0
+
+    while True:
+        nextParents = set([mod for mod, value in modules.items() if set(value['dest']) & rxParents])
+        rxParents = nextParents
+        expectedPulse = 1- expectedPulse
+        if len(rxParents) > 1:
+            break
+    
+    cycles = {mod: 0 for mod in rxParents}
+
+    while True:
+        cycle += 1
+
+        # Initial pulse to broadcaster and all destinations
+        pulseCount[0] += len(modules['broadcaster']['dest']) + 1
+        
+        queue = [(modules['broadcaster']['dest'], 0, 'broadcaster')]
+        while queue:
+            dests, pulse, source = queue.pop(0)
+
+            for dest in dests:
+                if dest not in modules:
+                    continue
+
+                if modules[dest]['type'] == '%':
+                    if pulse == 0:
+                        modules[dest]['switch'] = not modules[dest]['switch']
+                        pulseType = int(modules[dest]['switch'])
+                        pulseCount[pulseType] += len(modules[dest]['dest'])
+                        queue.append((modules[dest]['dest'], pulseType, dest))
+                elif modules[dest]['type'] == '&':
+                    modules[dest]['inputs'][source] = pulse
+                    pulseType = 1 - all(modules[dest]['inputs'].values())
+                    pulseCount[pulseType] += len(modules[dest]['dest'])
+                    queue.append((modules[dest]['dest'], pulseType, dest))
+
+                if dest in cycles and cycles[dest] == 0 and pulseType == expectedPulse:
+                    cycles[dest] = cycle
+                
+                if all(cycles.values()):
+                    return math.lcm(*cycles.values())
+
+print(partOne())
+print(partTwo())
