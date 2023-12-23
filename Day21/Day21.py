c9223099225be1b9a0f5622d2aa1de5e515fd2c8
kristianverduin <kristianverduin@gmail.com>
2023-12-23 20:25:49 +0100

    Day 21 part 2

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from collections import deque
+
+# Part 1 takeStep can still be improved a lot.. - Part 1 very slow
 
 def readInput():
     with open("input.txt", 'r') as file:
@@ -9,9 +13,9 @@ def parseMap(input):
     rocks = []
     for i in range(len(input)):
         for j in range(len(input[0])):
-            if lines[i][j] == 'S':
+            if input[i][j] == 'S':
                 start = (i, j)
-            elif lines[i][j] == '#':
+            elif input[i][j] == '#':
                 rocks.append((i, j))
     return start, rocks
 
@@ -28,18 +32,66 @@ def takeStep(rocks, lenX, lenY, steps):
             newSteps.append((step[0], step[1]+1))
     return newSteps
 
+def partOne():
+    nrSteps = 0
+    maxSteps = 64
+    steps = []
+    steps.append(start)
+
+    while nrSteps < maxSteps:
+        steps = takeStep(rocks, sizeX, sizeY, steps)
+        nrSteps += 1
+
+    print(len(steps))
+
+def quadaticForumla(y, n):
+    a = (y[2] - (2*y[1]) + y[0]) // 2
+    b = y[1] - y[0] - a
+    c = y[0]
+    return (a*n**2) + (b*n) + c
+
+def possibleMoves(coord, map):
+    moves = []
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    for direction in directions:
+        newCoord = (coord[0] + direction[0], coord[1] + direction[1])
+        if map[newCoord[1] % 131][newCoord[0] % 131] != '#':
+            moves.append(newCoord)
+    return moves
+
+def bfs(start, map, maxDistance):
+    tiles = defaultdict(int)
+    visited = set()
+    unvisited = deque([(start, 0)])
+
+    while unvisited:
+        (x, y), distance = unvisited.popleft()
+        if distance == (maxDistance + 1) or (x,y) in visited:
+            continue
+        tiles[distance] += 1
+        visited.add((x, y))
+
+        for step in possibleMoves((x,y), map):
+            unvisited.append((step, distance+1))
+    return tiles
+
+def calculatePossibleSpots(start, map, maxSteps):
+    tiles = bfs(start, map, maxSteps)
+    return sum(x for y, x in tiles.items() if y % 2 == maxSteps % 2)
+
+def partTwo():
+    cycles = 26501365
+    size = len(lines)
+    edge = size // 2
+
+    y = [calculatePossibleSpots(start, lines, (edge + i * size)) for i in range(3)]
+
+    print(quadaticForumla(y, ((cycles - edge) // size)))
+
 lines = readInput()
 start, rocks = parseMap(lines)
 sizeX = len(lines[0])
 sizeY = len(lines)
 
-nrSteps = 0
-maxSteps = 64
-steps = []
-steps.append(start)
-
-while nrSteps < maxSteps:
-    steps = takeStep(rocks, sizeX, sizeY, steps)
-    nrSteps += 1
-
-print(len(steps))
+partOne()
+partTwo()
