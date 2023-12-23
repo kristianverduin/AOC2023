bb614269ba536f33d2e51f5cf6f04869e913d3d1
kristianverduin <kristianverduin@gmail.com>
2023-12-23 19:37:21 +0100

    Day 23 part 2

--- Day23/Day23.py
+++ Day23/Day23.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import math
+from collections import deque
 
 directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 slopes = "><v^"
@@ -39,7 +39,61 @@ def readInput():
         text = file.read()
     return text.strip().split('\n')
 
-lines = readInput()
-start = [(i, j) for i in range(1) for j in range(len(lines[0])) if lines[i][j] == '.']
-end = [(i, j) for i in range(len(lines)-1, len(lines)) for j in range(len(lines[0])) if lines[i][j] == '.']
-print(walk(lines, start, end))
+def partOne():
+    lines = readInput()
+    start = [(i, j) for i in range(1) for j in range(len(lines[0])) if lines[i][j] == '.']
+    end = [(i, j) for i in range(len(lines)-1, len(lines)) for j in range(len(lines[0])) if lines[i][j] == '.']
+    print(walk(lines, start, end))
+
+def dfs(graph, start, end):
+    distance = 0
+    unvisited = deque([(start, 0, {start})])
+    
+    while unvisited:
+        node, currentDistance, visited = unvisited.popleft()
+        if node == end:
+            distance = max(distance, currentDistance)
+            continue
+        for neighbor, weight in graph[node].items():
+            if neighbor not in visited:
+                newDistance = currentDistance + weight
+                newVisited = visited | {neighbor}
+                unvisited.append((neighbor, newDistance, newVisited))
+    return distance
+
+def findIntersectionDistances(graph, start, intersections):
+    distances = {}
+    visited = set()
+    unvisited = deque([(start, 0)])
+    while unvisited:
+        (x, y), distance = unvisited.popleft()
+        if (x, y) in intersections and (x, y) != start:
+            distances[(x, y)] = distance
+            continue
+        for newX, newY in findNeighbors(graph, x, y):
+            if (newX, newY) not in visited:
+                visited.add((newX, newY))
+                unvisited.append(((newX, newY), distance+1))
+    return {start: distances}
+
+def findIntersections(graph):
+    intersections = []
+    for i in range(len(graph[0])):
+        for j in range(len(graph)):
+            if graph[i][j] != '#' and len(findNeighbors(graph, i, j)) > 2:
+                intersections.append((i, j))
+    return intersections
+
+def partTwo():
+    lines = readInput()
+    start = [(i, j) for i in range(1) for j in range(len(lines[0])) if lines[i][j] == '.']
+    end = [(i, j) for i in range(len(lines)-1, len(lines)) for j in range(len(lines[0])) if lines[i][j] == '.']
+    intersections = findIntersections(lines)
+
+    nodes = start + intersections + end
+    graph = {}
+    for node in nodes:
+        graph |= findIntersectionDistances(lines, node, nodes)
+    print(dfs(graph, start[0], end[0]))
+
+partTwo()
